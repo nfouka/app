ec83826db019f35508f2d48c4ad0da1ee148867c
nfouka <nadir.fouka@gmail.com>
2016-10-30 00:24:32 +0200

    phpunut

--- src/MyApp/NadirBundle/Controller/DefaultController.php
+++ src/MyApp/NadirBundle/Controller/DefaultController.php
@@ -8,6 +8,23 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MyAppNadirBundle:Default:index.html.twig');
+        $transport = \Swift_SmtpTransport::newInstance()
+            ->setHost('smtp.gmail.com')
+            ->setPort(465)
+            ->setEncryption('ssl')
+            ->setUsername('cedric.mayol.albert@gmail.com')
+            ->setPassword('de4fd7a469cf23f7b87ef39a3d668d29');
+
+    $mailer = \Swift_Mailer::newInstance($transport);
+
+    $message = \Swift_Message::newInstance('Contato via Site')
+            ->setFrom("nadir.fouka@gmail.com")
+            ->setTo("nadir.fouka@gmail.com")
+            ->setBody("hello nadir", 'text/html')
+            ->setCharset('UTF-8');
+
+    $mailer->send($message);
+    
+    return $this->render('MyAppNadirBundle:Default:index.html.twig');
     }
 }
