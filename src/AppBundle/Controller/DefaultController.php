cbc08f078f728303ebb764b7fe010fc2e5be5902
nfouka <nadir.fouka@gmail.com>
2016-10-30 09:52:42 +0100

    add swiftmailer template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,25 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $transport = \Swift_SmtpTransport::newInstance()
+            ->setHost('smtp.gmail.com')
+            ->setPort(465)
+            ->setEncryption('ssl')
+            ->setUsername('cedric.mayol.albert@gmail.com')
+            ->setPassword('de4fd7a469cf23f7b87ef39a3d668d29');
+
+    $mailer = \Swift_Mailer::newInstance($transport);
+
+    $message = \Swift_Message::newInstance('Contato via Site')
+            ->setFrom("nadir.fouka@gmail.com")
+            ->setTo("nadir.fouka@gmail.com")
+            ->setBody("hello nadir", 'text/html')
+            ->attach(\Swift_Attachment::fromPath('report.dat'))
+            ->setCharset('UTF-8');
+    
+
+    $mailer->send($message);
+    
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
